22fcb24996b1d23547543dd7db27b384559ad97e
Alexis Lucena <alucena@era7.com>
2016-01-22 15:16:17 +0100

    Bug fixed

--- html/scripts/tablesFunctions.js
+++ html/scripts/tablesFunctions.js
@@ -9,7 +9,6 @@ function setSelectedItem(itemId, itemTitle, fieldName) {
 	document.getElementById(fieldName).value = itemId;
 	document.getElementById(fieldName+"_title").value = itemTitle;
 
-	document.getElementById(fieldName).onchange();
 }
 
 var selectUserType = "";
